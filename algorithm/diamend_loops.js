8de10ff4689e3ed3361ef4dc79f6cfe1ed0de213
bhavanasisoundarya <116268639+bhavanasisoundarya@users.noreply.github.com>
2023-02-07 18:58:40 +0530

    added js

--- algorithm/diamend_loops.js
+++ algorithm/diamend_loops.js
@@ -1,24 +1,24 @@
-let n = 4;
-let string = "";
-for (let i = 1; i <= n; i++) {
-for (let j = 1; j <= n - i; j++) {
-  string += " ";
-}
-for (let k = 1; k <= i; k++) {
-  string += i + " ";
-}
-string += "\n";
-}
-for (let i = n - 1; i >= 1; i--) {
-for (let j = 1; j <= n - i; j++) {
-  string += " ";
-}
-for (let k = 1; k <= i; k++) {
-  string += i + " ";
-}
-string += "\n";
-}
-console.log(string);
+// let n = 4;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+// for (let j = 1; j <= n - i; j++) {
+//   string += " ";
+// }
+// for (let k = 1; k <= i; k++) {
+//   string += i + " ";
+// }
+// string += "\n";
+// }
+// for (let i = n - 1; i >= 1; i--) {
+// for (let j = 1; j <= n - i; j++) {
+//   string += " ";
+// }
+// for (let k = 1; k <= i; k++) {
+//   string += i + " ";
+// }
+// string += "\n";
+// }
+// console.log(string);
 
 
 
@@ -63,3 +63,42 @@ console.log(string);
 
 
 
+
+
+
+let n = 5;
+// Repeat n times
+for(let i = 1; i <= n ; i++) {
+//	//spaces + sequence of numbers
+	console.log( getSpaces((n - i + 1)) + getNumSequence(i));
+}
+ 
+//console.log( getSpaces(3) + getNumSequence(1));
+
+
+//console.log( getSpaces(2) + getNumSequence(2));
+
+//console.log( getSpaces(1) + getNumSequence(3));
+
+function getSpaces(n) {
+	let str = "";
+	for(let i = 1; i <= n ; i++) {
+		str = str + " ";
+	}
+	return str;
+}
+
+
+function getNumSequence(n) {
+	
+	let numseq_str = "";
+	for(let i = 1; i <= n; i++) {
+		numseq_str = numseq_str + i + " ";
+
+	}
+	return numseq_str;
+}
+		
+//Reverse half diamond
+	//Repeat 2 times
+		// spaces + sequence in reverse of numbers
---
+++ pages/homepage/register and login.js
@@ -0,0 +1,53 @@
+// const signUp = e => {
+//     let email = document.getElementById("email").value
+//         mobile = document.getElementById("mobile").value
+//        full_name = document.getElementById("full_name").value
+//       psw = document.getElementById("psw").value
+//        confirmpassword = document.getElementById("TXTpassword").value
+//        //   // Parsing the JSON string back into an array, or creating an empty array if there's no data
+//    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+//    let exist = user_list.length &&
+//        JSON.parse(localStorage.getItem('user_list')).some(data =>
+//            data.mobile.toLowerCase() == mobile.toLowerCase() ||
+//            data.email.toLowerCase() == email.toLowerCase()
+//        );
+//    if(!exist){
+//        user_list.push({ email, phone_number, name, psw,
+//                     repeat });
+//        localStorage.setItem('user_list', JSON.stringify(user_list));
+//        console.log(localStorage.getItem('user_list'));
+//        document.querySelector('form').reset();
+//        document.getElementById('email').focus();
+//        document.getElementById('psw').focus();
+//        if( password == confirmpassword)
+//        {
+//            alert("Account created Successfully :white_check_mark:");
+//             location.href="./homepage/login.html";
+//        }
+//        else{
+//            alert("Invalid Confirm password  :x:");
+//        }
+//    }
+//    else{
+//        alert('Sorry the User already Exist  ..:x:!! \n Try with different Phone number or email');
+//        document.querySelector('form').reset();
+//    }
+//    e.preventDefault();
+// }
+// const signIn = e =>{
+//         let email = document.getElementById('email');
+//        let  psw = document.getElementById('psw').value;
+//       let   user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+//            let exist = user_list.length &&
+//        JSON.parse(localStorage.getItem('user_list')).some(data =>
+//            data.email.toLowerCase() == email &&
+//            data.psw.toLowerCase() == psw);
+//        if(!exist){
+//            alert("Incorrect login credentials..:x:");
+//        }
+//        else{
+//            alert("Your login in successful ..:white_check_mark:");
+//            location.href = "./user_interface.html";
+//        }
+//        e.preventDefault();
+//    }
